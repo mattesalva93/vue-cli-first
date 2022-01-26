c6af89b5d481be4d31af0ff02a3905cbe207330a
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-01-26 15:12:45 +0100

    title done

---
+++ nuovaapp/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <qualsiasinomebastachenonsiatitle/>
+  </div>
+</template>
+
+<script>
+import qualsiasinomebastachenonsiatitle from './components/qualsiasinomebastachenonsiatitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    qualsiasinomebastachenonsiatitle
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ nuovaapp/src/components/qualsiasinomebastachenonsiatitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h1>La mia prima App con Vue Cli!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'qualsiasinomebastachenonsiatitle',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
